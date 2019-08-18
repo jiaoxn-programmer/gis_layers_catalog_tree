b3eaa5fc6416c6ac18f903b50f5983f02fc27428
jiaoxn <nihaojxn.programmer@gmail.com>
2019-08-18 12:52:43 +0800

    添加区域查询、打印地图和上传文件按钮

--- src/components/function-button-group/FunctionButtonGroupComponent.vue
+++ src/components/function-button-group/FunctionButtonGroupComponent.vue
@@ -11,6 +11,15 @@
                 <el-tooltip content="框选统计" placement="top">
                     <el-button id="frameSelectionButton" icon="el-icon-frame-selection" size="small" @click="frameSelectionStatisticsOnEsriView"></el-button>
                 </el-tooltip>
+                <el-tooltip content="区域查询" placement="top">
+                    <el-button id="regionalQueryButton" icon="el-icon-regional-query" size="small" @click="regionalQueryOnEsriView"></el-button>
+                </el-tooltip>
+                <el-tooltip content="打印地图" placement="top">
+                    <el-button id="printButton" icon="el-icon-print" size="small" @click="printOnEsriView"></el-button>
+                </el-tooltip>
+                <el-tooltip content="上传文件" placement="top">
+                    <el-button id="uploadButton" icon="el-icon-upload" size="small" @click="uploadOnEsriView"></el-button>
+                </el-tooltip>
             </el-button-group>
         </div>
     </div>
--- src/components/function-button-group/function-button-group-component.js
+++ src/components/function-button-group/function-button-group-component.js
@@ -23,6 +23,24 @@ export default {
          */
         frameSelectionStatisticsOnEsriView: function () {
             this.$parent.handleFunctionButtonClick('frameSelectionStatistics')
+        },
+        /**
+         * 区域查询按钮的点击函数
+         */
+        regionalQueryOnEsriView: function () {
+            this.$parent.handleFunctionButtonClick('regionalQuery')
+        },
+        /**
+         * 打印地图按钮的点击函数
+         */
+        printOnEsriView: function () {
+            this.$parent.handleFunctionButtonClick('print')
+        },
+        /**
+         * 上传文件按钮的点击函数
+         */
+        uploadOnEsriView: function () {
+            this.$parent.handleFunctionButtonClick('upload')
         }
     },
     data () {
